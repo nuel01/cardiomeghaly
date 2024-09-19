0dad08146a2c3aa103cae6be394f30dae1e8326e
Nuel <adegunlehinabayomi@yahoo.com>
2024-09-19 12:37:47 -0500

    Add files via upload

---
+++ templates/partials/code1.py
@@ -0,0 +1,7 @@
+# import os
+# import sys
+# from sklearn.externals import joblib
+# loaded_model = joblib.load('piped.pkl')
+# loaded_text='cardiac silhouette is normal in size and contour. CTR of 0.47'
+
+# print(loaded_model.predict(loaded_text))
